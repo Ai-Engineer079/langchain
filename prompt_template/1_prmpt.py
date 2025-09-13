45bc6d986ff31524cf6ecc0a4d3c07bc7570cf03
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2025-09-13 13:39:19 +0500

    Switch to ChatPromptTemplate and add invocation example
    Replaced PromptTemplate with ChatPromptTemplate and added code to create and invoke a prompt for generating a professional email. The result is printed after invoking the LLM with the constructed prompt.

--- prompt_template/1_prmpt.py
+++ prompt_template/1_prmpt.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 
 load_dotenv()
@@ -10,3 +10,16 @@ llm = ChatOpenAI(model='gpt-4')
 template = """write a {tone} email to {company}
 expressing  in {position} position, mentioning {skill} as a key strength.
 keep it {length} words long."""
+
+
+prompt_template = ChatPromptTemplate.from_template(template)
+
+prompt = prompt_template.invoke({
+    "tone": "professional",
+    "company" : "Google",
+    "position": "AI Engineer",
+    "skill": "Python programming",
+    "length" :100
+})
+result = llm.invoke(prompt)
+print(result.content) 
